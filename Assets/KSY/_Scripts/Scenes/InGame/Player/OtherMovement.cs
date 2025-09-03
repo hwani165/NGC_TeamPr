f64960f045f9bcf819fbb36ecf944e6b5d6a2f6e
김민 <pounl46v1@gmail.com>
2025-09-03 19:39:31 +0900

    feat : removeSameBool

--- Assets/KSY/_Scripts/Scenes/InGame/Player/OtherMovement.cs
+++ Assets/KSY/_Scripts/Scenes/InGame/Player/OtherMovement.cs
@@ -56,6 +56,7 @@ public class OtherMovement : Player
             _dashTimer = 0f;
         }
     }
+
     private void Update()
     {
 
@@ -129,25 +130,21 @@ public class OtherMovement : Player
     {
         if (!CanDash)
         {
+
+            Vector2 inputDir = _moveVec.normalized;
             if (_isGrounded)
             {
                 _rbCompo.linearVelocityX = 0;
-                CanDash = true;
-            }
-            else
-            {
-                CanDash = false;
-                _rbCompo.linearVelocity = Vector2.zero;
-            }
-            if (_isDashing) return;
-
-            Vector2 inputDir = _moveVec.normalized;
-
-            if (_isGrounded)
-            {
+                //CanDash = true;
                 if (inputDir == Vector2.zero)
                     inputDir = Vector2.down;
             }
+            else
+            {
+                //CanDash = false;
+                _rbCompo.linearVelocity = Vector2.zero;
+            }
+            if (_isDashing) return;
             else
             {
                 inputDir = new Vector2(Mathf.Sign(_moveVec.x), 0);
