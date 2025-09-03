61a2a38ea75867cfece35e7ad39e82b8027fda7e
SiriusSquare <dhehrud777@gmail.com>
2025-09-03 20:23:49 +0900

    Add MakeCreate HP Bar.

--- Assets/ODK/Entity.cs
+++ Assets/ODK/Entity.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    public float MaxHP { get; private set; } = 100f;
-    public float CurrentHp { get; private set; } = 100f;
-    [SerializeField] private HPBar hpbar;
+    public float MaxHP { get; private set; } = 0f;
+    public float CurrentHp { get; private set; } = 0f;
+    [SerializeField] private CircleHpBarScript hpbar;
     public void Attack(Transform tra, float damage, float knockback)
     {
 
-        CurrentHp -= damage;
-        hpbar.SetHP();
+        CurrentHp += damage;
+        hpbar.SetHP(CurrentHp);
 
         GetComponent<Rigidbody2D>().AddForce(new Vector2(transform.position.normalized.x - tra.position.normalized.x * knockback *5,knockback * 10),ForceMode2D.Impulse);
     }
