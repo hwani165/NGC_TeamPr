f2e327c19aff9cfef1bae110a59502ed9f3266a9
SiriusSquare <dhehrud777@gmail.com>
2025-09-04 19:12:56 +0900

    Fixed Knockback
    s

---
+++ Assets/ODK/01.Script/TimerScript.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    private TextMeshProUGUI timerText;
+    private float maxTime = 180f;
+    [SerializeField] private float elapsedTime;
+    private bool isRunning;
+    private bool isReversed = true;
+    private void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+        StartTimer();
+        UpdateTimerDisplay();
+    }
+
+    private void Update()
+    {
+        if (isReversed)
+        {
+            elapsedTime -= Time.deltaTime;
+            if (elapsedTime < 0f)
+            {
+                elapsedTime = 0f;
+                isReversed = false;
+            }
+            UpdateTimerDisplay();
+            return;
+        }
+        if (isRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            UpdateTimerDisplay();
+        }
+    }
+
+    public void StartTimer()
+    {
+        elapsedTime = 6f;
+        isRunning = true;
+        UpdateTimerDisplay();
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+        if (elapsedTime > maxTime - 30)
+        {
+            timerText.color = Color.red;
+        }
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+
+}
