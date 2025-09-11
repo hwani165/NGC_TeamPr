70a3c107d813946da064d63aca8a9a828d2d51fd
SiriusSquare <dhehrud777@gmail.com>
2025-09-11 18:55:15 +0900

    Add Scene . CountDown

--- Assets/ODK/01.Script/TimerScript.cs
+++ Assets/ODK/01.Script/TimerScript.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class TimerScript : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class TimerScript : MonoBehaviour
     private bool isRunning;
     private bool isReversed = true;
     [SerializeField] private GameObject countdownTimer;
+    private bool isCountdownStarted = false;
+
+    [SerializeField] private CountDownScript countDownScript;
     private void Start()
     {
         timerText = GetComponent<TextMeshProUGUI>();
@@ -40,30 +44,51 @@ public class TimerScript : MonoBehaviour
     {
         isRunning = false;
     }
+    [ContextMenu("Timer EndRed")]
+    public void TimerEndRed()
+    {
+        elapsedTime = maxTime - 8;
+        UpdateTimerDisplay();
+    }
 
     private void UpdateTimerDisplay()
     {
         int minutes = Mathf.FloorToInt(elapsedTime / 60f);
         int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+
         if (elapsedTime > maxTime - 30)
         {
             timerText.color = Color.red;
         }
-        if (elapsedTime > maxTime - 5)
+
+        if (!isCountdownStarted && elapsedTime > maxTime - 5)
         {
+            isCountdownStarted = true;
             countdownTimer.SetActive(true);
-            StartCoroutine(CountDown((int)maxTime - (int)elapsedTime));
+            StartCoroutine(CountDown(5));
         }
+
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
-    public IEnumerator CountDown(int i)
-    {
-        countdownTimer.GetComponent<TextMeshProUGUI>().DOKill();
-        countdownTimer.GetComponent<TextMeshProUGUI>().text = i.ToString();
-        countdownTimer.GetComponent<TextMeshProUGUI>().color = new Color(1f, 0f, 0f, 1f);
-        countdownTimer.GetComponent<TextMeshProUGUI>().DOColor(new Color(1f,0f,0f,0f), 0.8f).SetEase(Ease.OutElastic);
 
-        yield return new WaitForSeconds(1f);
+    public IEnumerator CountDown(int secondsLeft)
+    {
+        var tmp = countdownTimer.GetComponent<TextMeshProUGUI>();
+        while (secondsLeft > 0)
+        {
+            
+            tmp.DOKill();
+            tmp.text = secondsLeft.ToString();
+            tmp.color = new Color(1f, 0f, 0f, 1f);
+            tmp.DOColor(new Color(1f, 0f, 0f, 0f), 0.8f).SetEase(Ease.InOutSine);
+
+            yield return new WaitForSeconds(1f);
+            secondsLeft--;
+        }
+        tmp.color = new Color(1f, 0f, 0f, 1f);
+        tmp.text = "FINISH!";
+        StopTimer();
+        StartCoroutine(countDownScript.FadingSlideClose());
     }
 
 }
